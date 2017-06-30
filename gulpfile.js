e2aadb740cb69127b7af7bd62175952998af7f80
tounga franck <tounga franck>
2017-06-30 16:26:10 +0200

    Mise en place du serveur front complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,49 @@
 // Required
 
-var gulp = require('gulp'),
-uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 var wiredep = require('wiredep').stream;
 var inject = require('gulp-inject');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
+
+gulp.task('concat-js', function(){
+	gulp.src('scripts/**/*.js')
+	.pipe(concat('all.js'))
+	.pipe(gulp.dest('dist/script'));
+});
+
+gulp.task('concat-css', function(){
+	gulp.src('styles/**/*.css')
+	.pipe(concat('all.css'))
+	.pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('serve', ['default'],function(){
+	browserSync.init({
+		server: {
+     		baseDir: "./dist",
+    routes: {
+        "/bower_components": "bower_components",
+        "/dist": "dist",
+        "/scripts": "scripts",
+        "/images": "images"
+    }
+}
+	});
+});
 
 gulp.task('watch', function(){
-	gulp.watch(['scripts/**/*.js', '*.html'], ['inject']);
+	gulp.watch(['scripts/**/*.js', 'styles/**/*.css','*.html'], ['inject']).on("change", reload);
 });
 
 gulp.task('inject', function() {
     gulp.src('index.html')
         .pipe(wiredep({}))
-        .pipe(inject(gulp.src(['scripts/**/*.js', 'styles/**/*.css'], {read: false})))
+        .pipe(inject(gulp.src(['dist/**/*.js', 'dist/**/*.css'], {read: false})))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['inject','watch']);
+gulp.task('default', ['concat-js','concat-css','inject','watch']);
--- readme.md
+++ readme.md
@@ -12,3 +12,6 @@ Le client de l'api de gestion documentaire.
 Once you cloned the project: git clone https://github.com/ftounga/GedClient.git
 
 Command line to type: npm install -g bower ; npm install -g gulp ; npm install in your workspace directory.
+
+And then:
+	- gulp serve. Il will launch a server at 3000 port. 
